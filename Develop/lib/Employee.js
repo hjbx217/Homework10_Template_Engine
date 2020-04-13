c15ad4069910cfdff5c6d61f511a4f7dc6ce98f1
Hailey Bae <haejinbae@gmail.com>
2020-04-12 21:56:57 -0400

    added finished tests

--- Develop/lib/Employee.js
+++ Develop/lib/Employee.js
@@ -1,1 +1,38 @@
 // TODO: Write code to define and export the Employee class
+class Employee {
+
+    constructor(name, id, email) {
+        this.name = name;
+        this.id = id;
+        this.email = email;
+    }
+
+    /*printInfo() {
+        console.log(`name: ${this.name}`);
+        console.log(`id: ${this.id}`);
+        console.log(`email: ${this.email}`);
+    }*/
+    
+    //create functions for name, id, email
+    getName() {
+        return this.name    
+    }
+
+    getId() {
+        return this.id
+    }
+
+    getEmail() {
+        return this.email
+    }
+
+    getRole() {
+        return "Employee"
+    }
+
+}
+
+module.exports = Employee;
+
+
+//create functions or methods, need to create functions to get the name ID & Email. Last one is returning the string for employee
